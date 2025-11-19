3606dfeee01c8ccb2224e327bc6fc6a2f7dbd547
JGSaMueLrm <1559447@senati.pe>
2025-11-18 22:51:18 -0500

    Ultimas actualizaciones

--- js/main.js
+++ js/main.js
@@ -16,6 +16,7 @@ menuLinks.forEach(link => {
     });
 });
 
+
 // FORMULARIO DE CONTACTO
 const contactForm = document.getElementById('contactForm');
 if (contactForm) {
@@ -38,4 +39,28 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
             });
         }
     });
+});
+
+// EFECTO DE APARICIÓN AL HACER SCROLL
+const faders = document.querySelectorAll('.fade-in');
+const appearOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px -100px 0px"
+};
+const appearOnScroll = new IntersectionObserver(function (
+    entries,
+    appearOnScroll
+) {
+    entries.forEach(entry => {  
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            entry.target.classList.add('appear');
+            appearOnScroll.unobserve(entry.target);
+        }
+    });
+}, appearOptions);
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
 });
